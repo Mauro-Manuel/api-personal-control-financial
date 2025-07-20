5700d6c1cd46959357762fa2c2523328f75307d4
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-07-20 19:29:41 +0100

    feat: add RecipeFilterDTO

--- src/main/java/com/masprog/dto/RecipeFilterDTO.java
+++ src/main/java/com/masprog/dto/RecipeFilterDTO.java
@@ -1,2 +1,62 @@
-package com.masprog.dto;public class RecipeFilterDTO {
+package com.masprog.dto;
+
+import com.masprog.model.RecipeOrigin;
+
+import java.time.LocalDate;
+
+public class RecipeFilterDTO {
+    private RecipeOrigin origin;
+    private String destination;
+    private LocalDate receivedDateFrom;
+    private LocalDate receivedDateTo;
+    private Integer month;
+    private Integer year;
+
+    public RecipeOrigin getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(RecipeOrigin origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public LocalDate getReceivedDateFrom() {
+        return receivedDateFrom;
+    }
+
+    public void setReceivedDateFrom(LocalDate receivedDateFrom) {
+        this.receivedDateFrom = receivedDateFrom;
+    }
+
+    public LocalDate getReceivedDateTo() {
+        return receivedDateTo;
+    }
+
+    public void setReceivedDateTo(LocalDate receivedDateTo) {
+        this.receivedDateTo = receivedDateTo;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
 }
