1aeae07f23c297f7a5a1647a5b3b9a0054b47a1d
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-07-20 19:32:25 +0100

    feat: add getAllRecipes on RecipeService

--- src/main/java/com/masprog/service/RecipeService.java
+++ src/main/java/com/masprog/service/RecipeService.java
@@ -1,13 +1,16 @@
 package com.masprog.service;
 
 import com.masprog.dto.RecipeDTO;
+import com.masprog.dto.RecipeFilterDTO;
 import com.masprog.dto.RecipeResponseDTO;
 import com.masprog.exceptions.RequiredObjectIsNullException;
-import com.masprog.mapper.ObjectMapper;
 import com.masprog.model.Recipe;
 import com.masprog.repository.RecipeRepository;
+import com.masprog.repository.RecipeSpecification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,5 +35,15 @@ public class RecipeService {
         logger.info("Creating one Recipe!");
         Recipe recipe = parseObject(recipeDTO, Recipe.class);
         return  parseObject(recipeRepository.save(recipe), RecipeResponseDTO.class);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<RecipeResponseDTO> getAllRecipes(RecipeFilterDTO filter, Pageable pageable){
+        logger.info("Retrieving recipes with filters and pagination");
+        Page<Recipe> recipePage = recipeRepository
+                .findAll(RecipeSpecification.withFilters(filter), pageable);
+
+        return recipePage.map(recipe -> parseObject(recipe, RecipeResponseDTO.class));
+
     }
 }
