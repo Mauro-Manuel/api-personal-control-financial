25abcde4f532bd52486e6e0a9328ac90bf9fdb0c
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-07-20 19:30:58 +0100

    feat: update RecipeRepository

--- src/main/java/com/masprog/repository/RecipeRepository.java
+++ src/main/java/com/masprog/repository/RecipeRepository.java
@@ -2,6 +2,8 @@ package com.masprog.repository;
 
 import com.masprog.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, Long>,
+        JpaSpecificationExecutor<Recipe> {
 }
