dd67d41a94c961ed4672a98dd20e87c945a490da
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-07-28 14:04:34 +0100

    feat: add interface getRecipeById on RecipeControllerDocs

--- src/main/java/com/masprog/controller/docs/RecipeControllerDocs.java
+++ src/main/java/com/masprog/controller/docs/RecipeControllerDocs.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -40,7 +41,7 @@ public interface RecipeControllerDocs {
                     @ApiResponse(
                             description = "Success",
                             responseCode = "200",
-                            content = @Content(schema = @Schema(implementation = RecipeDTO.class))),
+                            content = @Content(schema = @Schema(implementation = RecipeResponseDTO.class))),
                     @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
                     @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
@@ -49,6 +50,25 @@ public interface RecipeControllerDocs {
             }
     )
     ResponseEntity<Page<RecipeResponseDTO>> getAllRecipes(RecipeFilterDTO filter, Pageable pageable);
+
+
+    @Operation(summary = "Listar Receita pelo ID",
+            description = "Encontrar uma especifica receita pelo ID",
+            tags = {"Receita"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = RecipeResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    ResponseEntity<RecipeResponseDTO> getRecipeById(@PathVariable("id") Long id);
 
 
 
