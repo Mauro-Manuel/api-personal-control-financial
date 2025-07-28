3c3da7fe0253379511a5380e91a828234cb09a9c
Mauro Manuel <mauro.manuel@unitel.co.ao>
2025-07-28 14:13:16 +0100

    feat: add interface deleteRecipe in RecipeControllerDocs

--- src/main/java/com/masprog/controller/docs/RecipeControllerDocs.java
+++ src/main/java/com/masprog/controller/docs/RecipeControllerDocs.java
@@ -71,5 +71,21 @@ public interface RecipeControllerDocs {
     ResponseEntity<RecipeResponseDTO> getRecipeById(@PathVariable("id") Long id);
 
 
+    @Operation(summary = "Apagar uma receita",
+            description = "Apagar uma receita pelo ID",
+            tags = {"Receita"},
+            responses = {
+                    @ApiResponse(
+                            description = "No Content",
+                            responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    ResponseEntity<Void> deleteRecipe(@PathVariable("id") Long id);
+
+
 
 }
